2b08630e94744eab5624faf1af179f8ea0fd7eab
ljackson140 <llakujack@outlook.com>
2021-05-09 09:52:47 +1000

    rankings complete

--- src/components/rankings.jsx
+++ src/components/rankings.jsx
@@ -1,71 +1,154 @@
 import React, { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
-//import { Button } from "reactstrap";
+import { AgGridColumn } from 'ag-grid-react/lib/agGridColumn';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button } from 'reactstrap';
+import { Redirect } from 'react-router-dom';
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
+import "./ranking.css";
+import "./home.css";
 
-const API_URL = "http://131.181.190.87:3000";
-const url = `${API_URL}/rankings`;
+// const API_URL = "http://131.181.190.87:3000";
+// const url = `${API_URL}/rankings`;
 
-// const years = [
-//     {title: "2015", link: "?year=2015"},
-//     {title: "2016", link: "?year=2016"},
-//     {title: "2017", link: "?year=2017"},
-//     {title: "2018", link: "?year=2018"},
-//     {title: "2019", link: "?year=2019"},
-//     {title: "2020", link: "?year=2020"},
-//     {title: "2021", link: "?year=2021"},
-// ]
+const years = [
+    {title: "2015", link: "?year=2015"},
+    {title: "2016", link: "?year=2016"},
+    {title: "2017", link: "?year=2017"},
+    {title: "2018", link: "?year=2018"},
+    {title: "2019", link: "?year=2019"},
+    {title: "2020", link: "?year=2020"},
+    {title: "2021", link: "?year=2021"},
+]
 
 function Rankings() {
     
     const [rowData, setRowData] = useState([]);
-    // const [selected, setSelected] = useState("");
-    // const [year, setYear] = useState("");
+    const [selected, setSelected] = useState("");
+    const [year, setYear] = useState("");
+
+    // useEffect(() => {
+    //     fetch(url + year) 
+    //       .then((res) => res.json())
+    //       .then((data) =>
+    //         data.map((info) => {
+    //           return {
+    //             rank: info.rank,
+    //             country: info.country,
+    //             score: info.score,
+    //             year: info.year,
+    //           };
+    //         })
+    //       )
+    //       .then((info) => setRowData(info));
+    // }, [year]);
 
     useEffect(() => {
-        fetch(url)
-          .then((res) => res.json())
-          .then((data) =>
-            data.map((info) => {
-              return {
-                rank: info.rank,
-                country: info.country,
-                score: info.score,
-                year: info.year,
-              };
-            })
-          )
-          .then((info) => setRowData(info));
-    });
+        fetch("http://131.181.190.87:3000/rankings" + year)
+            .then(res => res.json())
+            .then(data =>
+                data.map(company => {
+                    return {
+                        rank: company.rank,
+                        country: company.country,
+                        score: company.score,
+                        year: company.year,
+                    };
+                })
+            )
+            .then(allCompanies => setRowData(allCompanies));
+        console.log(year);
+    }, [year]);
+
+    function setYearFunc() {
+        setYear(this.link);
+    }
+
+    function Dropdownmenu(){
+        const [dropdownOpen, setDropdownOpen] = useState(false);
+        const toggle = () => setDropdownOpen(prevState => !prevState);
+
+        return (
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                <DropdownToggle caret>
+                    Year
+                </DropdownToggle>
+                <DropdownMenu className="industry-drop">
+                    {years.map(data => (
+                        <React.Fragment key={data.title}>
+                            <DropdownItem onClick={setYearFunc.bind(data)}>{data.title}</DropdownItem>
+                        </React.Fragment>
+                    ))}
+                </DropdownMenu>
+            </Dropdown>
+        )
+    }
+
+    function clickHander(props) {
+        setSelected(props.data.year);
+    }
 
     const columns = [
-        { headerName: "Rank", field: "rank" },
-        { headerName: "Country", field: "country" },
-        { headerName: "Score", field: "score" },
-        { headerName: "Year", field: "year", sortable: true, filter: true },
-    ];
+        { headerName: "Rank", field: "rank", filter: "agTextColumnFilter", filter: true, sortable: true, width:"200", flex:"300" },
+        { headerName: "Country", field: "country", filter: "agTextColumnFilter", filter: true, sortable: true, width:"200", flex:"300" },
+        { headerName: "Score", field: "score", filter: "agTextColumnFilter", filter: true, sortable: true, width:"200", flex:"300" },
+        { headerName: "Year", field: "year", filter: "agTextColumnFilter", filter: true, sortable: true, width:"200", flex:"300" }
+      ];
 
-    return (
-        <div className="container">
-      <h1>Rankings</h1>
-      
-      <div
-        className="ag-theme-balham"
-        style={{
-          height: "700px",
-          width: "800px"
-        }}
-      >
-        <AgGridReact
-          columnDefs={columns}
-          rowData={rowData}
-          pagination={true}
-          paginationPageSize={30}
-        />
-      </div>
-    </div>
-    );
+    function Table(){
+        return (
+
+            <AgGridReact
+                columnDefs={columns}
+                rowData={rowData}
+                pagination={true}
+                paginationPageSize={30}
+                rowSelection={true}
+                onRowDoubleClicked={clickHander}
+            >
+            </AgGridReact>
+        );
+    }
+
+    if (selected === "") {
+        return (
+            <div>
+                <div className="jumbo">
+                    <div className="transbox">
+                        <div className="transMessage">
+                                <div className="title">
+                                    Rankings
+                                </div>
+                        </div>
+                    </div>
+                    <div
+                        className="ag-theme-alpine-dark table"
+                        style={{
+                        height: "60vh",
+                        width: "60vw",
+                        marginRight: "5%",
+                        marginLeft : "5%"
+                        }}
+                        >
+                        <div className="industry-drop">
+                            <Dropdownmenu />
+                        </div>
+                        <Table />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return(
+            <Redirect to={"./search" + selected} />
+        )
+    }
+
+
+
+   
 }
 
 export default Rankings;
